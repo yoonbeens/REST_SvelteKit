43b377da8d0540fbffc6635a2dde8a69f469004f
yoon <cunbn@naver.com>
2024-02-14 18:15:19 +0900

    네이버 로그인 구현중

--- Oauth.md
+++ Oauth.md
@@ -6,8 +6,25 @@
     - ResourceOwner
 
 - MainService
-    - Client
+    - Client(회원가입을 대신 하는 OAuth와 서비스를 유저에게 제공)
     
 - SubService
     - ResourceServer
 ```
+
+## Register
+
+- 사전 등록
+    - Client 는 Resource Server 의 API를 사용하여 Resource Owner의 정보를 이용하려면 미리 사전에 등록(register)해야 한다
+
+- Client ID
+    - 등록하고자 하는 Service App의 식별 정보
+    - 외부 노출 가능
+
+- Client Secret
+    - 등록하고자 하는 Service App의 식별 정보
+    - 외부 노출 불가(관리 필수)
+
+- Authorized redirect URIs
+    - 권한이 부여되면 key code를 보낼 등록되어 있는 Service URI
+    - kakao 등록 URI : http://localhost:5173/oauth/kakao/info
---
+++ src/routes/oauth/naver/+page.svelte
@@ -0,0 +1,109 @@
+
+<div class="container">
+    <div class="header clearfix">
+        <nav>
+            <ul class="nav nav-pills pull-right">
+                <li role="presentation" class="active"><a href="#">Home</a></li>
+                <li role="presentation"><a id="gnbLogin" href="#">Login</a></li>
+            </ul>
+        </nav>
+        <h3 class="text-muted">Login With NaverID Javascript SDK</h3>
+    </div>
+
+    <div class="jumbotron">
+        <h1>SAMPLE PAGE</h1>
+        <p class="lead">네이버 아이디로 로그인 Javascript 샘플 페이지.<br> 간단한 적용 예제를 포함합니다.</p>
+        <!-- (1) 버튼 event 처리를 위하여 id를 지정 id=loginButton -->
+        <p>
+            <div id="naverIdLogin"><a id="naverIdLogin_loginButton" href="#" role="button"><img src="https://static.nid.naver.com/oauth/big_g.PNG" width=320></a></div>
+        
+    </div>
+
+    <div class="row marketing">
+        <div class="col-lg-6">
+            <h4>네이버 아이디로 로그인</h4>
+            <p>별도의 아이디, 비밀번호없이 네이버 아이디로 간편하게 외부 서비스에 로그인 할 수 있도록 하는 서비스입니다. 이용자는 복잡하고 번거로운 회원 가입 절차 없이 편하게 서비스를 이용하고, 사업자는 회원 가입, 로그인에
+                대한 허들을 낮춰 회원수가 늘고 매출은 오르는 경험을 할 수 있습니다. </p>
+
+            <h4>다양한 사용자 정보를 손쉽게 조회</h4>
+            <p>이름, 이메일, 별명, 생일, 연령대, 성별 등을 API로 간단한 조회를 통해 많은 사용자의 정보를 손쉽게 얻을 수 있습니다. 또한 블로그 등 다양한 네이버 서비스 들로 매시업이 가능합니다. </p>
+        </div>
+
+        <div class="col-lg-6">
+            <h4>4,200만 네이버 회원을 여러분의 사용자로!</h4>
+            <p>네이버 회원이라면, 여러분의 사이트를 간편하게 이용할 수 있습니다. 전 국민 모두가 가지고 있는 네이버 아이디 한개만 있으면 별도 가입없이 어떤 플랫폼에서도 간편하게 로그인할 수 있습니다. </p>
+
+            <h4>빠른개발, 간편한 운영</h4>
+            <p>플랫폼 별 SDK를 제공하여 쉽고 빠르게 개발할 수 있도록 지원합니다. 또한 환경별 로그인, 누적 사용자 등 다양한 통계를 제공하여 성과를 확인할 수 있습니다. </p>
+        </div>
+    </div>
+
+    <footer class="footer">
+    </footer>
+
+</div>
+<!-- /container -->
+
+
+
+<!-- (3) LoginWithNaverId Javscript 설정 정보 및 초기화 -->
+<script>
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        window.name='opener';
+    var naverLogin = new naver.LoginWithNaverId(
+        {
+            clientId: "",
+            callbackUrl: "http://" + window.location.hostname + ((location.port==""||location.port==undefined)?"":":" + location.port) + "/oauth/naver/info",
+            isPopup: true,
+            loginButton: {color: "green", type: 3, height: 60}
+        }
+    );
+    /* (4) 네아로 로그인 정보를 초기화하기 위하여 init을 호출 */
+    naverLogin.init();
+    
+    /* (4-1) 임의의 링크를 설정해줄 필요가 있는 경우 */
+    // $("#gnbLogin").attr("href", naverLogin.generateAuthorizeUrl());
+    document.getElementById("gnbLogin").setAttribute("href", naverLogin.generateAuthorizeUrl());
+
+    /* (5) 현재 로그인 상태를 확인 */
+    window.addEventListener('load', function () {
+        naverLogin.getLoginStatus(function (status) {
+            if (status) {
+                /* (6) 로그인 상태가 "true" 인 경우 로그인 버튼을 없애고 사용자 정보를 출력합니다. */
+                setLoginStatus();
+            }
+        });
+    });
+
+    /* (6) 로그인 상태가 "true" 인 경우 로그인 버튼을 없애고 사용자 정보를 출력합니다. */
+    function setLoginStatus() {
+        var profileImage = naverLogin.user.getProfileImage();
+        var nickName = naverLogin.user.getNickName();
+        var imageViewer = '';
+        if (profileImage) {
+            imageViewer += '<br><br><img src="' + profileImage + '" height=50 /> <p>';
+        }
+        // $("#naverIdLogin_loginButton").html(imageViewer + nickName + '님 반갑습니다.</p>');
+        document.getElementById("naverIdLogin_loginButton").innerHTML = imageViewer + nickName + '님 반갑습니다.</p>';
+        // $("#gnbLogin").html("Logout");
+        document.getElementById("gnbLogin").innerHTML = "Logout";
+        // $("#gnbLogin").attr("href", "#");
+        document.getElementById("gnbLogin").setAttribute("href", "#");
+        /* (7) 로그아웃 버튼을 설정하고 동작을 정의합니다. */
+        // $("#gnbLogin").click(function (e) {
+        //     e.preventDefault();
+        //     naverLogin.logout();
+        //     location.replace('/oauth/sample/javascript_sample.html');
+        // });
+        document.getElementById("gnbLogin").addEventListener('click', function (e) {
+            e.preventDefault();
+            naverLogin.logout();
+            location.replace('/oauth/naver');
+        });
+    }
+    });
+
+
+</script>
